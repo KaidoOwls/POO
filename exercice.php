6be069990ef62a843dc280acc5ddc9e5c8a050c8
KaidoOwls <seydina.diallo60@gmail.comseydina.diallo60@gmail.comgit config --global user.email seydina.diallo60@gmail.com>
2023-02-21 11:41:52 +0100

    first commit

---
+++ exercice.php
@@ -0,0 +1,19 @@
+<?php
+class animal 
+{
+    public $espece,
+    private $taille;
+    private $poids;
+    private $nbpattes;
+
+    public function manger() {
+        // ...
+    }
+
+    public function avancer(int $nbpas) {
+        // ...
+    }
+
+
+
+}
