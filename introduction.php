6fc18e52514dbd92bba75ac14aaaefce77313e0a
KaidoOwls <seydina.diallo60@gmail.comseydina.diallo60@gmail.comgit config --global user.email seydina.diallo60@gmail.com>
2023-02-22 11:39:24 +0100

    modif

---
+++ introduction.php
@@ -0,0 +1,32 @@
+<?php
+class Employe {
+
+    public $nom;
+    public $prenom;
+    public $age;
+
+    public function __construct($prenom, $nom, $age)
+    {   
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->age = $age;
+     
+    }
+
+    public function presentation() {
+        var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans");
+
+    }
+
+
+
+
+}
+
+$employe1 = new Employe("Diallo","Seydina", 23);
+
+
+$employe2 = new Employe("Malek", "Julien", 22);
+
+$employe1->presentation();
+$employe2->presentation();
